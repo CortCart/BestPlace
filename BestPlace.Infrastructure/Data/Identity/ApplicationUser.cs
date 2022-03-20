ef15daa0c13287738a798792eed4f38675e93e16
Stefan147 Stefan147 <Stefan147@students.softuni.bg>
2022-03-20 22:21:37 +0200

    extend IdentityUser

---
+++ BestPlace.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace BestPlace.Infrastructure.Data.Identity;
+
+public class ApplicationUser:IdentityUser
+{
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [StringLength(50)]
+    public string LastName { get; set; }
+}
